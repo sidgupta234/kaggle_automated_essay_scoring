088909c3f07031a0d3d68ebc0e825e5852043fa9
Siddharth Gupta <siddharthgupta234@gmail.com>
2022-11-14 04:38:25 +0530

    add tester modified

--- tester.py
+++ tester.py
@@ -9,8 +9,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+# Vocabulary
 pickled_model_vocab = pickle.load(open('vocab.pkl', 'rb'))
-pickled_model_convention = pickle.load(open('convention.pkl', 'rb'))
 
 dataset = pd.read_csv('data/test.csv')
 english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
@@ -82,52 +82,93 @@ print("--- %s seconds ---" % (end_time - start_time))
 
 # Linear Regression
 
-lr_vocab = pickled_model_vocab.predict(dataset)
-lr_score = lr_vocab.score(X_train, y_train)
-
 # print("Results for Linear Regression with Train Data")
 # print(lr_score)
+X = dataset[['word_frequency_score', 'avg_unique_words_per_total_words', 'avg_word_length']]
 
-lr_score = lr_vocab.score(X_test, y_test)
+predictions = pickled_model_vocab.predict(X)
 
-# print("Results for Linear Regression with Test Data")
-# print(lr_score)
+# # print("Results for Linear Regression with Test Data")
+# # print(lr_score)
 
-y_train_predict = lr_vocab.predict(X_train)
-y_train_predict = [(round(i*2)/2) for i in y_train_predict]
-print("rmse", mean_squared_error(y_train, y_train_predict))
-# y_train_list = y_train.to_list()
+y_train_predict = [(round(i*2)/2) for i in predictions]
+print(y_train_predict)
 
-# total = 0
-# correct = 0
+# Conventions
 
-# for i in range(len(y_train_list)):
-#     total += 1
-#     #print(y_test[i])
-#     if(y_train_list[i] == y_train_predict[i]):
-#         correct +=1 
-#         #print(y_test_list[i], y_predict[i])
+pickled_model_conventions = pickle.load(open('convention.pkl', 'rb'))
+dataset = pd.read_csv('data/test.csv')
+english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
 
-# print(total, correct)
-# print("percentage correct values in train are {:.2f}%".format(100*(correct/total)))
-# #print(y_predict)
+import time
+start_time = time.time()
 
-y_predict = lr_vocab.predict(X_test)
-y_predict = [(round(i*2)/2) for i in y_predict]
-print("rmse", mean_squared_error(y_test, y_predict))
-y_test_list = y_test.to_list()
+dataset = pd.read_csv('data/test.csv')
+english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
+english_vocab_frequency_dict =  dict(zip(english_vocab_frequency['word'], english_vocab_frequency['count']))
 
-total = 0
-correct = 0
+def clean_dataset(dataset_row):
+    #dataset_row["full_text"] = dataset_row["full_text"].lower()
+    dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
+    #dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+    dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
+    dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
+    return dataset_row['full_text']
 
-for i in range(len(y_test_list)):
-    total += 1
-    #print(y_test[i])
-    if(y_test_list[i] == y_predict[i]):
-        correct +=1 
-        #print(y_test_list[i], y_predict[i])
+def spell_error_per_total_words(dataset_row, english_vocab_frequency_dict):
+    dataset_row["full_text"] = dataset_row["full_text"].lower()
+    dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+    words = dataset_row["full_text"].split()
+    errors = 0
+    
+    for word in words:
+        if(word not in english_vocab_frequency_dict):
+            errors += 1
+            
+    return errors/len(words)
 
-print(correct, total)
-print("percentage correct values in test are {:.2f}%".format(100*(correct/total)))
-pickle.dump(lr_vocab, open('vocab.pkl', 'wb'))
+def punctuation_violations_per_sentence(dataset_row):
+    violations = 0
+    dataset_row["full_text"] = re.sub('[^0-9a-z.]', ' ', dataset_row["full_text"])
+    text_wo_spaces = "".join(dataset_row["full_text"].split())
+    #print("asdasdas", text_wo_spaces)
+    #stop_count = 0
+    for i in range(len(text_wo_spaces)-1):
+        if(text_wo_spaces[i]=='.'):
+            #stop_count += 1
+            if(text_wo_spaces[i+1].islower()):
+                violations += 1
+                
+    #print(violations, stop_count, dataset_row['conventions'])
+    return 1/(violations+1)
 
+
+def avg_sentence_length(dataset_row):
+    #dataset_row["full_text"] = dataset_row["full_text"].lower()
+    dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
+    dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+    dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
+    dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
+    return len(dataset_row['full_text'].split())
+    
+    
+dataset['full_text'] = dataset.apply(clean_dataset, axis = 1)
+#print(dataset['full_text'][2])
+dataset['text_length'] = dataset.apply(lambda x: len(x['full_text'].split()), axis = 1)
+dataset['spell_error_rate'] = dataset.apply(lambda x: spell_error_per_total_words(dataset_row = x, english_vocab_frequency_dict = english_vocab_frequency_dict), axis = 1)
+dataset['violations_per_sentence'] = dataset.apply(punctuation_violations_per_sentence, axis = 1)
+dataset['avg_sentence_length'] = dataset.apply(avg_sentence_length, axis = 1) 
+#dataset['vocab_label'] = dataset.apply(vocab_label, axis = 1)
+end_time = time.time()
+
+# Train Test Split
+
+X = dataset[['spell_error_rate', 'violations_per_sentence', 'avg_sentence_length']]
+
+predictions = pickled_model_conventions.predict(X)
+
+# # print("Results for Linear Regression with Test Data")
+# # print(lr_score)
+
+y_train_predict = [(round(i*2)/2) for i in predictions]
+print(y_train_predict)
