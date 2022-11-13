eca0130cd4ce54344d37739640872d4c8da60e78
Siddharth Gupta <siddharthgupta234@gmail.com>
2022-11-14 05:08:37 +0530

    basic streamlit app

---
+++ app.py
@@ -0,0 +1,176 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+from tester import compute_vocab_conventions_score
+#from billboard_seaborn import *
+st.set_page_config(page_title='Music Dashboard', page_icon = "favicon.png", layout = 'wide', initial_sidebar_state = 'auto')
+footer="""<style>
+a:link , a:visited{
+color: white;
+background-color: transparent;
+text-decoration: underline;
+}
+a:hover,  a:active {
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: green;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p><b>Developed by <a style='text-align: left;' href="https://www.languageof.me/" target="_blank">Siddharth Gupta</a> for music aficionados</b>
+</p>
+</div>
+"""
+#st.set_page_config(layout="wide")
+c1, c2 = st.columns((2, 1))
+header = st.container()
+dataset = st.container()
+spotify_features = st.container()
+st.markdown(footer, unsafe_allow_html=True)
+#modelTraining = st.container()
+
+with header and c1:
+     st.title("How good are your Vocabulary and Conventions?")
+
+with c2:
+    a = st.text_input("Enter your Essay to get it rated", value="Sample Text")
+    df = pd.DataFrame([a], columns=["full_text"])
+    #print(df)
+    values = compute_vocab_conventions_score(df)
+    vocab = "Vocabulary score is " + str(values[0][0])
+    conventions = "Conventions score is " + str(values[1][0])
+    st.text(vocab)
+    st.text(conventions)
+#with dataset and c1:
+
+#     song_iframe = st.empty()
+#     # st.header("Billboard Top 100 Lyrics dataset")
+#     # st.text("Billboard dataset augmented with Lyrics using Genius API")
+#     file_path = "data/lyrics_lemmatized.csv"
+#     music_data = preprocess__(file_path)
+#     # music_data = pd.read_csv(file_path,  index_col=None)
+#     # #st.write(music_data.head())
+#     #st.write(music_data.index)
+#     # st.write(music_data.columns)
+#     # #music_data["performer+song"] = music_data["performer"] + "^" + music_data["song"]
+#     music_data = music_data.set_index('performer+song')
+
+#    # st.write(music_data.head())
+#     #st.write(music_data.index)
+#     #st.write(music_data.columns)
+#     #df = pd.read_csv('data.csv')  
+#     #set_of_artists = ""
+#     set_of_artists = set(music_data['performer'].tolist())
+#     artist_selected = st.selectbox('Select Artist', set_of_artists)
+#     #st.write((music_data[music_data["performer"].str.contains(artist_selected)]))
+    
+#     # if(set_of_artists != ""):
+#     artist_rows_with_aliases = music_data[music_data['performer'].str.contains(artist_selected)]
+#     #st.write(artist_rows_with_aliases["performer"])
+#     select_songs_by_artist_aliases = set(artist_rows_with_aliases['song'].tolist())
+#     song_selected = st.selectbox('Select Song by your Artist', select_songs_by_artist_aliases)
+#     #st.write()
+#    # st.write('|'.join(artist_rows_with_aliases["performer"]))
+#     row_to_print = music_data[(music_data['performer'].str.contains('|'.join(artist_rows_with_aliases["performer"]))) & (music_data["song"] == song_selected)].copy() #["Lyrics"]#==song_selected])]
+    
+#     #### Spotify_Song_ID_Details 
+#     spotify_song_id_details = ["songid", "spotify_genre", "spotify_track_id", 
+#     "spotify_track_preview_url", "spotify_track_duration_ms", "spotify_track_explicit", "spotify_track_album"]
+    
+#     # play_song = st.button('Play the selected track')
+
+#     # if(play_song):
+#     #     spotify_play_song(row_to_print[spotify_song_id_details])#[spotify_song_id_details])
+#     # ####
+
+#     recommend_songs_button = st.button('Playlist of songs like this!')
+
+#     # embed_link = "https://open.spotify.com/embed/playlist/2tzzmfQ9OL0r4Sc5hgLHtP"
+#     # components.iframe("https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3")
+
+#     #st.markdown('<html>hey</html>', unsafe_allow_html=True)
+
+#     # components.iframe(album_uri_link , width=600, height=200 )
+#     # st.markdown('<html><iframe src="" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media></iframe></html>', unsafe_allow_html=True)
+#     try:
+#         song_id = row_to_print['spotify_track_id'].iloc[0]
+#         embed_track_link = "https://open.spotify.com/embed/track/"+song_id
+#         #components.iframe(embed_track_link, width = 300, height = 380 , scrolling = True)
+#         song_iframe.markdown("<iframe src=" +str(embed_track_link)+" width = 300 height = 240></iframe>", unsafe_allow_html=True)
+#     except:
+#         pass
+    
+#     if(recommend_songs_button):
+#         list_of_recommended_songs = recommend_songs(row_to_print, music_data)
+#         #st.text(list_of_recommended_songs[['song', "performer"]].to_string(index=False))
+#         playlist_link = make_playlist(row_to_print.iloc[0]['song'], list_of_recommended_songs)
+
+#         html_for_link = "<p><a href="+str(playlist_link)+" style=color:#CDCDCD>Click here to go to the Playlist!</a></p> <div id='gtx-trans' style='position: absolute; left: 115px; top: -2px;'> <div class='gtx-trans-icon'></div> </div>"
+
+#         st.write((html_for_link), unsafe_allow_html=True)
+#         playlist_id = playlist_link.split("/")[-1]
+#         embed_playlist_link = "https://open.spotify.com/embed/playlist/"+playlist_id
+#         #components.iframe(embed_playlist_link)#, width = 300, height = 380 , scrolling = True)
+#         st.markdown("<iframe src=" +str(embed_playlist_link)+" width = 300 height = 240></iframe>", unsafe_allow_html=True)
+
+
+#     #iloc_index = artist_selected + "^" + song_selected
+#     #print(iloc_index)
+#     # st.text(row_to_print.iloc[0]["weekid"])
+#     # st.text(row_to_print["song"][0])
+#     # st.text(row_to_print["performer"][0])
+    
+#     #### Billboard Visualization
+#     billboard_features = ["weekid", "week_position", "song", "performer", "instance", "peak_position", "weeks_on_chart", "spotify_genre"]
+#     # st.text(row_to_print[billboard_features])
+
+# with dataset and c2:
+#     st.header("Lyrics for " +row_to_print["song"][0] + " by " +  row_to_print["performer"][0])
+#     #st.text("Lyrics are")
+#     #st.text(row_to_print["spotify_track_preview_url"][0])
+#     st.text(row_to_print["Lyrics"][0])
+#     #st.text(music_data[(music_data["performer"]==artist_selected) & (music_data["song"]==song_selected)]["Lyrics"])# & music_data["song"]==song_selected] )
+    
+# with dataset and c3:
+#     st.header("Billboard Ranking Vs Time")
+#     plt = billboard_viz_plot_weekid_vs_rank(row_to_print[billboard_features])
+#     st.pyplot(plt)
+#     #st.line_chart(row_to_print[billboard_features].iloc[0](columns={'date':'index'}).set_index('index'))
+#     # print(type(row_to_print))
+#     # print(row_to_print.columns)
+#     # row_to_print.set_index("weekid", inplace = True)
+#     # st.line_chart(row_to_print['week_position'].iloc[0])#, index="weekid")
+#     # ####
+
+# with dataset and c3:
+#     #### Spotify_Audio_Features_Visualised
+#     st.header("Audio properties of song")
+#     audio_features = ["speechiness", "acousticness", "liveness", "valence", "danceability", "energy"]
+#     #st.write(row_to_print.iloc[0])
+#     plt = viz_song_with_audio_features(row_to_print[audio_features])
+#     st.pyplot(plt)
+#     #####
+# Footer
+# © 2022 GitHub, Inc.
+# Footer navigation
+# Terms
+# Privacy
+# Security
+# Status
+# Docs
+# Contact GitHub
+# Pricing
+# API
+# Training
+# Blog
+# About
+# billboard-data-viz-and-lyrics-/app.py at main · sidgupta234/billboard-data-viz-and-lyrics-
--- tester.py
+++ tester.py
@@ -8,167 +8,170 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-
-# Vocabulary
-pickled_model_vocab = pickle.load(open('vocab.pkl', 'rb'))
-
-dataset = pd.read_csv('data/test.csv')
-english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
-
-import time
-start_time = time.time()
-
-dataset = pd.read_csv('data/test.csv')
-english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
-english_vocab_frequency_dict =  dict(zip(english_vocab_frequency['word'], english_vocab_frequency['count']))
-#print(len(english_vocab_frequency_dict))
 oov_list = []
 total_unique_words = []
-#sentence_number = 0
 
-#print(dataset['full_text'][0])
+# Vocabulary
 
-def vocab_clean_dataset(dataset_row):
-    #dataset_row["full_text"] = dataset_row["full_text"].lower()
-    dataset_row["full_text"] = dataset_row["full_text"].lower()
-    dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
-    dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
-    dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
-    dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
-    return dataset_row['full_text']
+def compute_vocab_conventions_score(text):
+    pickled_model_vocab = pickle.load(open('vocab.pkl', 'rb'))
 
-def vocab_avg_unique_words(dataset_row):
-    dict_ = {}
-    words = dataset_row['full_text'].split()
-    for word in words:
-        dict_[word] = 1
+    dataset = text#pd.read_csv('data/test.csv')
+    english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
+
+    import time
+    start_time = time.time()
+
+    dataset = text#pd.read_csv('data/test.csv')
+    english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
+    english_vocab_frequency_dict =  dict(zip(english_vocab_frequency['word'], english_vocab_frequency['count']))
+    #print(len(english_vocab_frequency_dict))
+    #sentence_number = 0
+
+    #print(dataset['full_text'][0])
+
+    def vocab_clean_dataset(dataset_row):
+        #dataset_row["full_text"] = dataset_row["full_text"].lower()
+        dataset_row["full_text"] = dataset_row["full_text"].lower()
+        dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
+        dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+        dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
+        dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
+        return dataset_row['full_text']
+
+    def vocab_avg_unique_words(dataset_row):
+        dict_ = {}
+        words = dataset_row['full_text'].split()
+        for word in words:
+            dict_[word] = 1
+            
+        return len(dict_)/len(words)
+
+    
+    def vocab_word_freq_score(dataset_row, english_vocab_frequency_dict):
+        global oov_list
+        global total_unique_words
+        freq_score = 0
+        #print(dataset_row)
+        words = dataset_row['full_text'].split()
+        global sentence_number
         
-    return len(dict_)/len(words)
-
-def vocab_word_freq_score(dataset_row, english_vocab_frequency_dict):
-    global oov_list
-    global total_unique_words
-    freq_score = 0
-    #print(dataset_row)
-    words = dataset_row['full_text'].split()
-    global sentence_number
-    
-#     if(sentence_number%50 == 0):
-#         print("sentencer_number is ", sentence_number)
-#     sentence_number+=1
-    for word in words:
-#         print(word)
-        if word in english_vocab_frequency_dict:
-            #print(english_vocab_frequency[english_vocab_frequency['word'] == word])
-            total_unique_words.append(word)
-            freq_score += math.exp(1/english_vocab_frequency_dict[word])
-        else:
-            #print(word)
-            oov_list.append(word)
-            #freq_score += 1/math.log(english_vocab_frequency_dict['the'])
-            #freq_score += math.log(1/english_vocab_frequency_dict[word])
-            #print(word)
-            
-    return math.log(freq_score)
-       
-dataset['full_text'] = dataset.apply(vocab_clean_dataset, axis = 1)
-dataset['text_length'] = dataset.apply(lambda x: len(x['full_text'].split()), axis = 1)
-dataset['avg_word_length'] = dataset.apply(lambda x: statistics.mean([len(i) for i in x['full_text'].split()]), axis = 1)
-dataset['avg_unique_words_per_total_words'] = dataset.apply(vocab_avg_unique_words, axis = 1)
-dataset['word_frequency_score'] = dataset.apply(lambda x: vocab_word_freq_score(dataset_row = x, english_vocab_frequency_dict = english_vocab_frequency_dict), axis = 1)
-#dataset['vocab_label'] = dataset.apply(vocab_label, axis = 1)
-end_time = time.time()
-print("--- %s seconds ---" % (end_time - start_time))
-
-# Linear Regression
-
-# print("Results for Linear Regression with Train Data")
-# print(lr_score)
-X = dataset[['word_frequency_score', 'avg_unique_words_per_total_words', 'avg_word_length']]
-
-predictions = pickled_model_vocab.predict(X)
-
-# # print("Results for Linear Regression with Test Data")
-# # print(lr_score)
-
-y_train_predict = [(round(i*2)/2) for i in predictions]
-print(y_train_predict)
-
-# Conventions
-
-pickled_model_conventions = pickle.load(open('convention.pkl', 'rb'))
-dataset = pd.read_csv('data/test.csv')
-english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
-
-import time
-start_time = time.time()
-
-dataset = pd.read_csv('data/test.csv')
-english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
-english_vocab_frequency_dict =  dict(zip(english_vocab_frequency['word'], english_vocab_frequency['count']))
-
-def clean_dataset(dataset_row):
-    #dataset_row["full_text"] = dataset_row["full_text"].lower()
-    dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
-    #dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
-    dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
-    dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
-    return dataset_row['full_text']
-
-def spell_error_per_total_words(dataset_row, english_vocab_frequency_dict):
-    dataset_row["full_text"] = dataset_row["full_text"].lower()
-    dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
-    words = dataset_row["full_text"].split()
-    errors = 0
-    
-    for word in words:
-        if(word not in english_vocab_frequency_dict):
-            errors += 1
-            
-    return errors/len(words)
-
-def punctuation_violations_per_sentence(dataset_row):
-    violations = 0
-    dataset_row["full_text"] = re.sub('[^0-9a-z.]', ' ', dataset_row["full_text"])
-    text_wo_spaces = "".join(dataset_row["full_text"].split())
-    #print("asdasdas", text_wo_spaces)
-    #stop_count = 0
-    for i in range(len(text_wo_spaces)-1):
-        if(text_wo_spaces[i]=='.'):
-            #stop_count += 1
-            if(text_wo_spaces[i+1].islower()):
-                violations += 1
+    #     if(sentence_number%50 == 0):
+    #         print("sentencer_number is ", sentence_number)
+    #     sentence_number+=1
+        for word in words:
+    #         print(word)
+            if word in english_vocab_frequency_dict:
+                #print(english_vocab_frequency[english_vocab_frequency['word'] == word])
+                total_unique_words.append(word)
+                freq_score += math.exp(1/english_vocab_frequency_dict[word])
+            else:
+                #print(word)
+                oov_list.append(word)
+                #freq_score += 1/math.log(english_vocab_frequency_dict['the'])
+                #freq_score += math.log(1/english_vocab_frequency_dict[word])
+                #print(word)
                 
-    #print(violations, stop_count, dataset_row['conventions'])
-    return 1/(violations+1)
+        return math.log(freq_score)
+        
+    dataset['full_text'] = dataset.apply(vocab_clean_dataset, axis = 1)
+    dataset['text_length'] = dataset.apply(lambda x: len(x['full_text'].split()), axis = 1)
+    dataset['avg_word_length'] = dataset.apply(lambda x: statistics.mean([len(i) for i in x['full_text'].split()]), axis = 1)
+    dataset['avg_unique_words_per_total_words'] = dataset.apply(vocab_avg_unique_words, axis = 1)
+    dataset['word_frequency_score'] = dataset.apply(lambda x: vocab_word_freq_score(dataset_row = x, english_vocab_frequency_dict = english_vocab_frequency_dict), axis = 1)
+    #dataset['vocab_label'] = dataset.apply(vocab_label, axis = 1)
+    end_time = time.time()
+    print("--- %s seconds ---" % (end_time - start_time))
+
+    # Linear Regression
+
+    # print("Results for Linear Regression with Train Data")
+    # print(lr_score)
+    X = dataset[['word_frequency_score', 'avg_unique_words_per_total_words', 'avg_word_length']]
+
+    predictions = pickled_model_vocab.predict(X)
+
+    # # print("Results for Linear Regression with Test Data")
+    # # print(lr_score)
+
+    y_train_predict_vocabulary = [(round(i*2)/2) for i in predictions]
+    #print(y_train_predict_vocabulary)
+
+    # Conventions
+
+    pickled_model_conventions = pickle.load(open('convention.pkl', 'rb'))
+    dataset = text#'data/test.csv')
+    english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
+
+    import time
+    start_time = time.time()
+
+    dataset = text#'data/test.csv')
+    english_vocab_frequency =  pd.read_csv('vocabulary_data/unigram_freq.csv')
+    english_vocab_frequency_dict =  dict(zip(english_vocab_frequency['word'], english_vocab_frequency['count']))
+
+    def clean_dataset(dataset_row):
+        #dataset_row["full_text"] = dataset_row["full_text"].lower()
+        dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
+        #dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+        dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
+        dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
+        return dataset_row['full_text']
+
+    def spell_error_per_total_words(dataset_row, english_vocab_frequency_dict):
+        dataset_row["full_text"] = dataset_row["full_text"].lower()
+        dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+        words = dataset_row["full_text"].split()
+        errors = 0
+        
+        for word in words:
+            if(word not in english_vocab_frequency_dict):
+                errors += 1
+                
+        return errors/len(words)
+
+    def punctuation_violations_per_sentence(dataset_row):
+        violations = 0
+        dataset_row["full_text"] = re.sub('[^0-9a-z.]', ' ', dataset_row["full_text"])
+        text_wo_spaces = "".join(dataset_row["full_text"].split())
+        #print("asdasdas", text_wo_spaces)
+        #stop_count = 0
+        for i in range(len(text_wo_spaces)-1):
+            if(text_wo_spaces[i]=='.'):
+                #stop_count += 1
+                if(text_wo_spaces[i+1].islower()):
+                    violations += 1
+                    
+        #print(violations, stop_count, dataset_row['conventions'])
+        return 1/(violations+1)
 
 
-def avg_sentence_length(dataset_row):
-    #dataset_row["full_text"] = dataset_row["full_text"].lower()
-    dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
-    dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
-    dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
-    dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
-    return len(dataset_row['full_text'].split())
-    
-    
-dataset['full_text'] = dataset.apply(clean_dataset, axis = 1)
-#print(dataset['full_text'][2])
-dataset['text_length'] = dataset.apply(lambda x: len(x['full_text'].split()), axis = 1)
-dataset['spell_error_rate'] = dataset.apply(lambda x: spell_error_per_total_words(dataset_row = x, english_vocab_frequency_dict = english_vocab_frequency_dict), axis = 1)
-dataset['violations_per_sentence'] = dataset.apply(punctuation_violations_per_sentence, axis = 1)
-dataset['avg_sentence_length'] = dataset.apply(avg_sentence_length, axis = 1) 
-#dataset['vocab_label'] = dataset.apply(vocab_label, axis = 1)
-end_time = time.time()
+    def avg_sentence_length(dataset_row):
+        #dataset_row["full_text"] = dataset_row["full_text"].lower()
+        dataset_row["full_text"] = re.sub('[\t\n\r]', ' ', dataset_row["full_text"]) #Removing \n, \t, \r from from full_test
+        dataset_row["full_text"] = re.sub('[^0-9a-z]', ' ', dataset_row["full_text"]) #Replace all symbols except a-z and 0-9 with spaces
+        dataset_row["full_text"] = re.sub('\s{2,}', ' ', dataset_row["full_text"]) #Replacing two or more spaces with single space
+        dataset_row['full_text'] = dataset_row["full_text"].strip() #Removing start or end of line spaces from full_test
+        return len(dataset_row['full_text'].split())
+        
+        
+    dataset['full_text'] = dataset.apply(clean_dataset, axis = 1)
+    #print(dataset['full_text'][2])
+    dataset['text_length'] = dataset.apply(lambda x: len(x['full_text'].split()), axis = 1)
+    dataset['spell_error_rate'] = dataset.apply(lambda x: spell_error_per_total_words(dataset_row = x, english_vocab_frequency_dict = english_vocab_frequency_dict), axis = 1)
+    dataset['violations_per_sentence'] = dataset.apply(punctuation_violations_per_sentence, axis = 1)
+    dataset['avg_sentence_length'] = dataset.apply(avg_sentence_length, axis = 1) 
+    #dataset['vocab_label'] = dataset.apply(vocab_label, axis = 1)
+    end_time = time.time()
 
-# Train Test Split
+    # Train Test Split
 
-X = dataset[['spell_error_rate', 'violations_per_sentence', 'avg_sentence_length']]
+    X = dataset[['spell_error_rate', 'violations_per_sentence', 'avg_sentence_length']]
 
-predictions = pickled_model_conventions.predict(X)
+    predictions = pickled_model_conventions.predict(X)
 
-# # print("Results for Linear Regression with Test Data")
-# # print(lr_score)
+    # # print("Results for Linear Regression with Test Data")
+    # # print(lr_score)
 
-y_train_predict = [(round(i*2)/2) for i in predictions]
-print(y_train_predict)
+    y_train_predict_conventions = [(round(i*2)/2) for i in predictions]
+    return y_train_predict_vocabulary, y_train_predict_conventions
